8a8ac2a836f7da23aff638567ed450583584a103
MaziVincent <georgevincent35@gmail.com>
2024-10-20 21:49:48 +0100

    login changes

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -78,25 +78,17 @@ const Login = () => {
   }, [persist])
   console.log(auth)
   return (
-    <div className=" bg-gray-50 dark:bg-gray-900 min-h-screen">
+    <div className=" bg-gray-100 dark:bg-gray-900 dark:text-white min-h-screen">
       <ToastContainer />
       <div className="contaoner mx-auto max-w-7xl py-14 px-4">
         <div className=" sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className=" mt-6 text-center text-3xl font-bold tracking-tight text-gray-700 dark:text-gray-300">
             Sign in to your account
           </h2>
-          <p className=" mt-2 text-center text-sm text-gray-600 dark:text-gray-300">
-            <span>Or </span>
-            <Link
-              to="/signup"
-              className=" font-medium underline hover:no-underline text-gray-700 hover:text-gray-800 dark:text-gray-300"
-            >
-              sign up for a new account
-            </Link>
-          </p>
+         
         </div>
-        <div className=" mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div className=" bg-white dark:bg-gray-800 dark:border dark:border-gray-700 shadow rounded-lg">
+        <div className=" mt-8 sm:mx-auto sm:w-full sm:max-w-md  ">
+          <div className=" dark:bg-gray-700 bg-gray-10  shadow rounded-lg">
             <div className=" p-8">
               <form
                 onSubmit={handleSubmit(login)}
@@ -105,7 +97,7 @@ const Login = () => {
                 <div className=" space-y-6">
                   <div>
                     <label
-                      className=" block text-sm font-medium text-gray-700 dark:text-gray-300"
+                      className=" block text-base font-medium text-gray-700 dark:text-white"
                       htmlFor="user_email"
                     >
                       Email address
@@ -113,7 +105,7 @@ const Login = () => {
                     <div className="input mt-2 relative rounded-md shadow-sm">
                       <input
                         placeholder="Email Address"
-                        className=" dark:bg-gray-700 block w-full rounded-md border py-3 px-3 focus:outline-none border-gray-300 focus:ring-blue-600 focus:border-blue-600"
+                        className="rounded-md px-2 py-3 bg-gray-50 dark:bg-gray-700 dark:text-white"
                         required="required"
                         type="email"
                         {...register("email", 
@@ -132,7 +124,7 @@ const Login = () => {
 
                   <div className="">
                     <label
-                      className=" block text-sm font-medium text-gray-700 dark:text-gray-300"
+                      className=" block text-base font-medium text-gray-700 dark:text-white"
                       htmlFor="password"
                     >
                       Password
@@ -140,7 +132,7 @@ const Login = () => {
                     <div className="input mt-2 relative rounded-md shadow-sm">
                       <input
                         placeholder="*********"
-                        className=" dark:bg-gray-700 block w-full rounded-md border py-3 px-3 focus:outline-none border-gray-300 focus:ring-blue-600 focus:border-blue-600"
+                        className=" dark:bg-gray-700 dark:text-white block w-full rounded-md border py-3 px-3 focus:outline-none border-gray-300 focus:ring-blue-600 focus:border-blue-600"
                         required="required"
                         type="password"
                         {...register("password",{required : true },
@@ -163,7 +155,7 @@ const Login = () => {
                     <div className=" flex items-center">
                       
                       <input
-                        className=" form-checkbox text-teal-700 focus:ring-teal-800 h-4 w-4 border-gray-300 rounded"
+                        className=" form-checkbox text-teal-700 focus:ring-teal-800 h-4 w-4 border-gray-50 rounded"
                         type="checkbox"
                         name=""
                         id="remember me"
@@ -171,15 +163,15 @@ const Login = () => {
                         checked={persist}
                       />
                       <label
-                        className=" ml-2 text-sm block text-gray-900 dark:text-gray-300 mb-0"
+                        className=" ml-2 text-base block text-gray-900 dark:text-gray-50 mb-0"
                         htmlFor="remember-me"
                       >
                         Remember me
                       </label>
                     </div>
 
-                    <div className=" text-sm">
-                      <Link className=" font-medium text-gray-600 dark:text-gray-300 underline">
+                    <div className=" text-base">
+                      <Link className=" font-medium text-gray-600 dark:text-gray-50 underline">
                         Forgot your password{" "}
                       </Link>
                     </div>
@@ -188,7 +180,7 @@ const Login = () => {
                   <div className="">
                     <button
                       type='submit'
-                      className={`cursor-pointer border rounded-md border-gray-300 bg-teal-800 text-white w-full py-2 px-2 text-sm`}
+                      className={`cursor-pointer border rounded-md border-gray-300 bg-teal-800 text-white w-full py-2 px-2 text-base`}
                     >
                       {isLoading ? <CircularProgress size={20} /> : 'Sign in'}
                     </button>
@@ -196,40 +188,8 @@ const Login = () => {
                 </div>
               </form>
 
-              <div className=" mt-6">
-                <div className=" relative">
-                  <div className=" absolute inset-0 flex items-center">
-                    <div className=" w-full border-t border-gray-300"></div>
-                  </div>
-                  <div className=" relative flex justify-center text-sm">
-                    <p className=" bg-white dark:bg-gray-800 px-2 text-gray-500 dark:text-gray-300">
-                      {" "}
-                      Or continue with
-                    </p>
-                  </div>
-                </div>
-              </div>
+              
 
-              <div className=" mt-6 grid grid-cols-2 gap-3">
-                <div className="">
-                  <form action="">
-                    <button className=" border rounded-md border-gray-300 w-full py-2 px-4 text-sm font-medium text-gray-500 dark:text-gray-300 focus:ring-0">
-                      <span className=" sr-only">Sign in with Facebook</span>
-                      <FacebookOutlined />
-                    </button>
-                   
-                  </form>
-                </div>
-
-                <div className="">
-                  <form action="">
-                    <button className=" border rounded-md border-gray-300 w-full py-2 px-4 text-sm font-medium text-gray-500 dark:text-gray-300 focus:ring-0">
-                      <span className=" sr-only">SIgn in with Google</span>
-                      <Google />
-                    </button>
-                  </form>
-                </div>
-              </div>
             </div>
           </div>
         </div>
