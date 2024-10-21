77c4c72007c2d923f287c9757764a2d3d20ca3a9
MaziVincent <georgevincent35@gmail.com>
2024-10-21 15:56:55 +0100

    added origins

--- back_End/config/allowedOrigins.js
+++ back_End/config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'https://www.mysite.com', 
     'http://127.0.0.1:5500', 
     'http://localhost:3500',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://mega-real-estate-group.vercel.app/'
 ]
 
 module.exports = allowedOrigins
