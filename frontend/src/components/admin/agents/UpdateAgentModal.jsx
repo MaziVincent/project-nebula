f6441224c40a9e0770acf1a5b77710f73fb941c9
Mz-Shed <shedrachmkpuma1@gmail.com>
2024-11-08 16:13:21 +0100

    commit

--- frontend/src/components/admin/agents/UpdateAgentModal.jsx
+++ frontend/src/components/admin/agents/UpdateAgentModal.jsx
@@ -336,16 +336,10 @@ const UpdateAgentModal = ({ openUpdate, handleUpdateClose, agent }) => {
                     id="whatsappLink"
                     name='whatsappLink'
                     type='text'
-                    {...register("whatsappLink", { required: true })}
+                    {...register("whatsappLink")}
                     className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-green-500 focus:border-primary-500 "
                     placeholder="Enter Office Adress here"
                   />
-                  {errors.whatsappLink && (
-                    <span className="text-red-500 text-sm">
-                      
-                      Field can't be empty
-                    </span>
-                  )}
                 </div>
                 
                 <div className="sm:col-span-2">
