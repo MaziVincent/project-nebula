1a5173ef6479e4390706e8a9125187f86d93d8aa
MaziVincent <georgevincent35@gmail.com>
2024-11-24 10:54:55 +0100

    dccc

--- frontend/api/property.js
+++ frontend/api/property.js
@@ -12,30 +12,31 @@ console.log(id)
   const response = await fetch(`${baseURL}/properties/${id}`);
   const propertyData = await response.json();
     console.log(propertyData)
-  // Render the PropertyDetails component to HTML
-  const appString = ReactDOMServer.renderToString(
-    React.createElement(PropertyDetails, { data: propertyData })
-  );
+//   // Render the PropertyDetails component to HTML
+//   const appString = ReactDOMServer.renderToString(
+//     React.createElement(PropertyDetails, { data: propertyData })
+//   );
 
   // Inject into an HTML template
-  const html = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta name="description" content="${propertyData?.description}">
-            <meta property="og:title" content="${propertyData?.title}" />
-            <meta property="og:url" content="${window.location.href}" />
-            <meta property="og:image" content="${propertyData?.imageUrls[0]}" />
-            <title>${propertyData.title}</title>
-        </head>
-        <body>
-            <div id="root">${appString}</div>
-        </body>
-        </html>
-    `;
+//   const html = `
+//         <!DOCTYPE html>
+//         <html lang="en">
+//         <head>
+//             <meta charset="UTF-8">
+//             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//             <meta name="description" content="${propertyData?.description}">
+//             <meta property="og:title" content="${propertyData?.title}" />
+//             <meta property="og:url" content="${window.location.href}" />
+//             <meta property="og:image" content="${propertyData?.imageUrls[0]}" />
+//             <title>${propertyData.title}</title>
+//         </head>
+//         <body>
+//             <div id="root">${appString}</div>
+//         </body>
+//         </html>
+//     `;
 
   res.setHeader("Content-Type", "text/html");
-  res.status(200).send(html);
+//   res.status(200).send(html);
+  res.status(200).json(propertyData);
 }
