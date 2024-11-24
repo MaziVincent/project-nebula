63f0d30e4cb50883cff30899e31367f09eec69aa
MaziVincent <georgevincent35@gmail.com>
2024-11-24 21:39:41 +0100

    nb mb

--- frontend/api/property.js
+++ frontend/api/property.js
@@ -4,7 +4,7 @@ import fetch from "node-fetch";
 
 // Adjust the paths as needed for your project structure
 import baseURL from "../src/shared/baseURL.js";
-import PropertyDetails from "../src/components/Home/PropertyDetails.js";
+import PropertyDetails from "./PropertyDetails.js";
 
 export default async function handler(req, res) {
   const { id } = req.query;
