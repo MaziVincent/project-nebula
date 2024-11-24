3fd5b0ced4297307f806c38d79f9aa7d0669ec53
MaziVincent <georgevincent35@gmail.com>
2024-11-24 05:53:55 +0100

    com

--- frontend/api/property/[id].js
+++ frontend/api/property/[id].js
@@ -1,3 +1,13 @@
+
+export default function handler(req, res) {
+  const { id } = req.query; // Extract 'id' from the URL
+  if (!id) {
+      res.status(400).json({ error: 'ID is required' });
+      return;
+  }
+  res.status(200).json({ message: `Property ID: ${id}` });
+}
+
 // import baseURL from "../../src/shared/baseURL";
 
 // const React = require("react");
@@ -40,11 +50,4 @@
 //   res.status(200).send(html);
 // }
 
-export default function handler(req, res) {
-  const { id } = req.query; // Extract 'id' from the URL
-  if (!id) {
-      res.status(400).json({ error: 'ID is required' });
-      return;
-  }
-  res.status(200).json({ message: `Property ID: ${id}` });
-}
+
