460f8bda3d27b0e55fa0f56d65239838d6d0cc59
Esomchi <georgevincent35@gmail.com>
2025-12-11 15:45:09 +0100

    updated build script

--- back_End/config/allowedOrigins.js
+++ back_End/config/allowedOrigins.js
@@ -1,15 +1,11 @@
 
 const allowedOrigins = [
-    'http://127.0.0.1:5500', 
-    'http://localhost:3500',
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'localhost:5173',
-    'https://mega-real-estate-group.vercel.app',
-    'https://megatechrealestate.ng',
-    'https://www.megatechrealestate.ng',
-    'https://www.mrg.ng',
-    'https://mrg.ng'
-]
+	"http://127.0.0.1:5500",
+	"http://localhost:3500",
+	"http://localhost:5173",
+	"http://localhost:5174",
+	"localhost:5173",
+	"https://project-nebula.pages.dev/",
+];
 
 module.exports = allowedOrigins
